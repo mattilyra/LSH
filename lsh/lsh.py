ecb018438610ac3a48f3822d4d7c9e2f5d9ba38a
Matti Lyra <m.lyra@sussex.ac.uk>
2015-10-16 12:41:05 +0200

    Refactored lsh.Cache so that computing the bins and buckets is a separate generator function. Makes the code a bit cleaner.

--- lsh/lsh.py
+++ lsh/lsh.py
@@ -57,7 +57,7 @@ class Cache(object):
         self.removed_articles = 0
         self.compress = compress
 
-    def update(self, fingerprint, meta=None):
+    def bins_(self, fingerprint):
         for bin_i, head in enumerate(range(0, len(self.bins))):
             # take r length vectors of minhashes from band i
             head = head * self.rows
@@ -65,64 +65,13 @@ class Cache(object):
                 raise RuntimeError('The number of bands * rows exceeds the \
 size of the fingerprint.')
             bucket = fingerprint[head:head + self.rows]
+            yield bin_i, bucket
+
+    def update(self, fingerprint, meta=None):
+        for bin_i, bucket in self.bins_(fingerprint):
             bucket_id = hash(tuple(bucket))
-            entry = (fingerprint, meta)
+            entry = (tuple(fingerprint), meta)
             self.bins[bin_i][bucket_id].add(entry)
-
-    def _neighbours(self, fingerprint):
-        bin_size = len(fingerprint) // len(self._bins)
-        for bin_i, head in enumerate(range(0, len(fingerprint), bin_size)):
-            # take r length vectors of minhashes from band i
-            head = head * self.rows
-            bucket = fingerprint[head:head + bin_size]
-            bucket_id = hash(tuple(bucket))
-            yield (n in self.bins[bin_i][bucket_id])
-
-    def is_duplicate(self, article, min_similarity=0.65,
-                     update=True, **kwargs):
-        """Checks if a document is a duplicate of an already seen document.
-
-        If the Jaccard similarity of two documents exceeds *min_similarity*
-        those two documents are deemed to be duplicates of each other.
-
-        The method returns a boolean indicating if the document is a
-        duplicate.
-
-        If a document is not a duplicate of anything in the cache it is added to
-        the cache by default. This behaviour can be turned off with the *update*
-        flag.
-        """
-
-        stripped_txt = article.replace('\n', ' ')
-        stripped_txt = re.sub('[\.,;:\-\+=&*!?><\(\)]*', '', stripped_txt)
-        stripped_txt = stripped_txt.lower()
-        stripped_txt = stripped_txt.encode('utf8').strip()
-        fingerprint = minhash(stripped_txt,
-                              len(stripped_txt), self._seeds, 8)
-
-        neighbours = self._neighbours(fingerprint)
-
-        ngram = self._ngram
-        end = len(stripped_txt) - ngram + 1
-        doc_shingles = set([stripped_txt[head:head + ngram] for head in
-                            range(0, end)])
-
-        for neighbour in neighbours:
-            neighbour_txt = zlib.decompress(neighbour)
-            end = len(neighbour_txt) - ngram + 1
-            neighbour_shingles = set([neighbour_txt[head:head + ngram]
-                                      for head in range(0, end)])
-
-            union = neighbour_shingles | doc_shingles
-            intersection = neighbour_shingles & doc_shingles
-            jaccard = len(intersection) / (len(union) + .0)
-            if jaccard >= min_similarity:
-                self.removed_articles += 1
-                return True
-
-        if update:
-            self._update(fingerprint, stripped_txt)
-        return False
 
 if __name__ == '__main__':
     lsh = Cache()
